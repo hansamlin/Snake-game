a203fbc319161a427ce5029a3f93350bff5c86cb
Sam <sam82520.work@gmail.com>
2020-08-09 19:43:35 +0800

    處理螢幕翻轉

--- src/components/useWindows.js
+++ src/components/useWindows.js
@@ -4,13 +4,15 @@ export default function useWindows() {
   const [size, setSize] = useState([window.innerWidth, window.innerHeight]);
 
   useEffect(() => {
-    function resize() {
+    function resize(e) {
       setSize([window.innerWidth, window.innerHeight]);
     }
     window.addEventListener('resize', resize);
+    window.addEventListener('orientationchange', resize);
 
     return () => {
       window.removeEventListener('resize', resize);
+      window.removeEventListener('orientationchange', resize);
     };
   }, []);
 
